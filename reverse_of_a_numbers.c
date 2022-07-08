9e0cd718c0234e7596ba6fc77081c4b0c3d1f56a
21A91A05E2 <97424487+21A91A05E2@users.noreply.github.com>
2022-07-08 21:02:43 +0530

    #include<stdio.h> int main() {     int n, remainder,sum=0;     scanf("%d",&n);     while(n!=0)     {         remainder=n%10;         sum=sum*10+remainder;         n=n/10;     }     printf("%d",sum);     return 0; }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n, remainder,sum=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        remainder=n%10;
+        sum=sum*10+remainder;
+        n=n/10;
+    }
+    printf("%d",sum);
+    return 0;
+}
